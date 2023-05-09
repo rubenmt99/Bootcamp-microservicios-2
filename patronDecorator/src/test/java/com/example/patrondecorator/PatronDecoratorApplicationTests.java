d6631b17d2a44123aa91d88464e2900fc9066569
rubenmt99 <matiastiscar@gmail.com>
2023-05-09 12:02:42 +0200

    primer commit

---
+++ patronDecorator/src/test/java/com/example/patrondecorator/PatronDecoratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patrondecorator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronDecoratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
