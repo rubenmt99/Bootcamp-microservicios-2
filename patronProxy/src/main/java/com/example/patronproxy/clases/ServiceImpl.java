7b0f337de09ad7f196b8a5e9dda98a3ea159b46c
rubenmt99 <matiastiscar@gmail.com>
2023-05-09 13:54:38 +0200

    fallo

--- patronProxy/src/main/java/com/example/patronproxy/clases/ServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.patronproxy.clases;
-
-public class ServiceImpl implements IService{
-    @Override
-    public void doSomeThing() {
-        System.out.println("Reproducirse");
-    }
-}
